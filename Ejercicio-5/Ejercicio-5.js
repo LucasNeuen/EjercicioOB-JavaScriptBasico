feea827cf66150c5ac39a02af6086d5330705f04
Lucas Garay <127149432+LucasNeuen@users.noreply.github.com>
2023-03-07 11:34:57 -0300

    Add files via upload

---
+++ Ejercicio-5/Ejercicio-5.js
@@ -0,0 +1,11 @@
+let height_cm = 183;
+let height = 1.83;
+let weight = 70.20;
+
+let height_entero = Math.ceil(height);
+
+
+let weight_entero = Math.floor(weight);
+
+
+let esIgual = Number.MAX_VALUE + 1 === Number.MAX_VALUE;
